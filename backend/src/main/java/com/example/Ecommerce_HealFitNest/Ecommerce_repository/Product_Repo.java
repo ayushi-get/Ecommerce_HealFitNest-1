aa31d823b39124c22f740d874071f6e70d31544c
Apoorva Nagar <60841039+APOORVANAGAR-2002@users.noreply.github.com>
2022-07-12 12:50:43 +0530

    Delete backend directory

--- backend/src/main/java/com/example/Ecommerce_HealFitNest/Ecommerce_repository/Product_Repo.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ecommerce_HealFitNest.Ecommerce_repository;
-
-import com.example.Ecommerce_HealFitNest.Ecommerce_model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Product_Repo extends MongoRepository<Product,Integer> {
-}
